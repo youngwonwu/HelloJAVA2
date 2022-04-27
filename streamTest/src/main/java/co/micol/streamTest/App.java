b2416bd201e8154280ced3ed77eaeec309ffac75
admin <admin@YD2-08>
2022-04-27 17:45:06 +0900

    Chapter 14 Main InputStream Writer Reader 출력

---
+++ streamTest/src/main/java/co/micol/streamTest/App.java
@@ -0,0 +1,25 @@
+package co.micol.streamTest;
+
+import co.micol.streamTest.exam.ReadExample;
+import co.micol.streamTest.exam.ReaderExample;
+import co.micol.streamTest.exam.WriteExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+//    	WriteExample wr = new WriteExample();
+//    	wr.run();
+//    	
+//    	ReadExample re = new ReadExample();
+//    	re.run();
+    	
+    	ReaderExample rs = new ReaderExample();
+    	rs.run();
+    	
+    }
+}
